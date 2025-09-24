96cacef3de5457e205577ec45699bd31fc286d2b
sota <sota.matsui@akane.waseda.jp>
2025-09-24 18:10:19 +0900

    feat: change hero design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { MobileNavigation, Navigation } from "@/components/nav";
+import { MobileNavigation } from "@/components/nav";
 import { AccessSection } from "@/components/sections/access";
 import { HeadMessageSection } from "@/components/sections/headMessage";
 import { ThemeSection } from "@/components/sections/theme";
@@ -11,22 +11,35 @@ export default function Home() {
       <div className="md:hidden p-4 sticky w-full top-0 z-50 bg-gradient-to-b via-80% from-background via-transparent to-transparent">
         <MobileNavigation />
       </div>
-      <section className="md:h-screen w-full md:bg-[url('/characters/background.png')] bg-cover bg-center">
-        <div className="relative flex justify-center h-full w-full">
-          <Image
-            src="/main_visual.png"
-            alt="餃子ダンス人間"
-            width={1543}
-            height={2183}
-            className="w-full md:h-full md:w-auto"
-          />
-          <Image
-            src="/logotype.svg"
-            alt="あきび祭2025ロゴ"
-            width={1030 / 2}
-            height={1677 / 2}
-            className="absolute top-0 right-0 mt-12 mr-4 md:m-16 h-auto max-w-[60vw] drop-shadow-[0_0_8px_white] [--tw-drop-shadow:var(--tw-drop-shadow-size)_var(--tw-drop-shadow-size)]"
-          />
+      <section className="md:h-screen w-full">
+        <div className="relative flex justify-start h-full w-full -space-x-16">
+          <div className="w-full md:w-auto md:h-full p-4">
+            <div className="w-full md:w-auto md:h-full rounded-4xl overflow-hidden">
+              <Image
+                src="/main_visual.png"
+                alt="あきび祭2025メインビジュアル"
+                width={1543}
+                height={2183}
+                className="w-full md:h-full md:w-auto md:aspect-square object-cover"
+              />
+            </div>
+          </div>
+          <div className="grow hidden md:flex justify-center items-center p-4">
+            <Image
+              src="/text-logo-white.svg"
+              alt="あきび祭2025ロゴ"
+              width={1030 / 2}
+              height={1677 / 2}
+            />
+          </div>
+          <div className="md:hidden absolute bottom-0 w-full px-16 translate-y-16">
+            <Image
+              src="/logotype.svg"
+              alt="あきび祭2025ロゴ"
+              width={1030 / 2}
+              height={1677 / 2}
+            />
+          </div>
         </div>
       </section>
       <YellowContainer className="[&>div]:min-h-screen">
