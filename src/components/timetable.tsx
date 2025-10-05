9581b252c786b43199d5db79f1393ccf57f4082a
Sota <sota.matsui@akane.waseda.jp>
2025-10-05 20:21:24 +0900

    feat:  improve timetable design

--- src/components/timetable.tsx
+++ src/components/timetable.tsx
@@ -37,17 +37,26 @@ const timeLabels = [
   "17:00",
 ];
 
+const TIMING_RANGE_START = 0;
+const TIMING_RANGE_END = 14;
+
+const isEventScheduledAllDay = (event: Schedule) => {
+  return event.timing_start === TIMING_RANGE_START && event.timing_end === TIMING_RANGE_END;
+}
+
 export default async function TimeTable({ events }: { events: Schedule[] }) {
-  const eventPlaces = Array.from(new Set(events.map((event) => event.place)));
+  const scheduledEvents = events.filter((event) => !isEventScheduledAllDay(event));
+  const allDayEvents = events.filter((event) => isEventScheduledAllDay(event));
+  const scheduledEventPlaces = Array.from(new Set(scheduledEvents.map((event) => event.place)));
   return (
     <div className="flex flex-col items-start gap-2">
       <section
         style={{
-          gridTemplateColumns: `3rem repeat(${eventPlaces.length}, minmax(0, 1fr))`,
+          gridTemplateColumns: `3rem repeat(${scheduledEventPlaces.length}, minmax(0, 1fr))`,
         }}
         className="max-h-[80vh] w-full mx-auto max-w-5xl grid gap-x-0.5"
       >
-        {eventPlaces.map((place, index) => {
+        {scheduledEventPlaces.map((place, index) => {
           return (
             <div
               key={place + index.toString()}
@@ -66,7 +75,7 @@ export default async function TimeTable({ events }: { events: Schedule[] }) {
       </section>
       <section
         style={{
-          gridTemplateColumns: `3rem repeat(${eventPlaces.length}, minmax(0, 1fr))`,
+          gridTemplateColumns: `3rem repeat(${scheduledEventPlaces.length}, minmax(0, 1fr))`,
         }}
         className="max-h-[80vh] w-full mx-auto max-w-5xl grid gap-x-0.5 grid-rows-14"
       >
@@ -94,14 +103,14 @@ export default async function TimeTable({ events }: { events: Schedule[] }) {
                 gridRowStart: index + 1,
                 gridRowEnd: index + 2,
                 gridColumnStart: 2,
-                gridColumnEnd: eventPlaces.length + 2,
+                gridColumnEnd: scheduledEventPlaces.length + 2,
               }}
               className="border-t-[1px] even:border-t-[2px] border-primary-foreground"
             />
           );
         })}
-        {events.map((event, index) => {
-          const placeIndex = eventPlaces.indexOf(event.place);
+        {scheduledEvents.map((event, index) => {
+          const placeIndex = scheduledEventPlaces.indexOf(event.place);
           return (
             <Dialog key={event.id + index.toString()}>
               <DialogTrigger asChild>
@@ -121,8 +130,8 @@ export default async function TimeTable({ events }: { events: Schedule[] }) {
               </DialogTrigger>
               <DialogContent>
                 <DialogHeader>
-                  <DialogTitle className="font-serif text-3xl flex items-center gap-4">
-                    <p>{event.name}</p>
+                  <DialogTitle className="font-serif text-3xl">
+                    {event.name}
                   </DialogTitle>
                   <DialogDescription>{event.organization}</DialogDescription>
                 </DialogHeader>
@@ -157,6 +166,63 @@ export default async function TimeTable({ events }: { events: Schedule[] }) {
           );
         })}
       </section>
+      {allDayEvents.length > 0 && (
+        <section className="w-full max-w-5xl mx-auto mt-4">
+          <h2 className="text-lg font-bold mb-2">終日開催イベント</h2>
+          <ul className="space-y-4">
+            {allDayEvents.map((event, index) => (
+              <Dialog key={event.id + index.toString()}>
+                <DialogTrigger asChild>
+                  <div
+                    className={`text-center text-xs font-bold my-0.5 rounded text-white flex items-center justify-center p-2 cursor-pointer hover:brightness-90 ${
+                      tableColors[
+                        index +
+                          (scheduledEventPlaces.length % tableColors.length)
+                      ]
+                    }`}
+                  >
+                    {event.name}
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="font-serif text-3xl">
+                      {event.name}
+                    </DialogTitle>
+                    <DialogDescription>{event.organization}</DialogDescription>
+                  </DialogHeader>
+                  <p
+                    className="text-sm font-bold"
+                    // biome-ignore lint/security/noDangerouslySetInnerHtml: using private CMS
+                    dangerouslySetInnerHTML={{
+                      __html: `${event.description.replace(/\n/g, "<br />")}`,
+                    }}
+                  />
+                  {event.place && (
+                    <div className="flex items-center gap-2">
+                      <MapPinIcon />
+                      <p>{event.place}</p>
+                    </div>
+                  )}
+                  <ul className="space-y-2">
+                    {event.days.map((day, dayIndex) => (
+                      <li
+                        key={day + dayIndex.toString()}
+                        className="flex items-center gap-2"
+                      >
+                        <Badge>{day}日目</Badge>
+                        <p className="text-sm font-bold">{`${
+                          timeLabels[event.timing_start]
+                        } 〜 ${timeLabels[event.timing_end]}`}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </DialogContent>
+              </Dialog>
+            ))}
+          </ul>
+        </section>
+      )}
     </div>
   );
 }
