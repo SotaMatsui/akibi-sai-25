1cefd306d8c40cd641cc1019144fe6994ab5525d
Sota <sota.matsui@akane.waseda.jp>
2025-10-11 16:56:21 +0900

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Kaisei_Opti, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
@@ -37,6 +38,7 @@ export default function RootLayout({
         <SmoothScroller />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
